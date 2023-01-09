c4c2ce3bc4ede143b41a56ce6401c78386a72036
Clyne Fuentes <clippefuentes@gmail.com>
2023-01-08 20:38:45 -0600

    add structure boxes

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,9 +13,66 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(child: Row(
+            children: [
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                height: 200.00,
+                width: 170.0,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 48, 47, 47),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              )),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                height: 200.00,
+                width: 170.0,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 48, 47, 47),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ))
+            ],
+          )),
+          Expanded(child: Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            height: 200.00,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 48, 47, 47),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ))),
+          Expanded(child: Row(
+            children: [
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                height: 200.00,
+                width: 170.0,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 48, 47, 47),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              )),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                height: 200.00,
+                width: 170.0,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 48, 47, 47),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ))
+            ],
+          )),
+        ],
+        
       ),
     );
   }
 }
+
+
